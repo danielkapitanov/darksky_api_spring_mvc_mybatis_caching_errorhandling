254d50d506f3366c5f34573676ce16cafa0ba0e1
Daniel Kapitanov <dskapitanov@gmail.com>
2017-08-14 10:21:48 +0700

    tweaked application for tomcat deployment

--- src/main/java/darkskyapi/Application.java
+++ src/main/java/darkskyapi/Application.java
@@ -2,7 +2,9 @@ package darkskyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.gemfire.CacheFactoryBean;
@@ -19,10 +21,15 @@ import java.util.Properties;
 @SpringBootApplication
 @EnableCaching
 @SuppressWarnings("unused")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
 //    @Bean
